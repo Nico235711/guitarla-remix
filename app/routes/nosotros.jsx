9a9f87efa9d9e58b6ab1f6e90f87293f38bca21c
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-02-23 16:14:08 -0300

    Creando la página de nosotros

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,8 +1,22 @@
+import nosotros from '../../public/img/nosotros.jpg'
 
 const Nosotros = () => {
   
   return (
-    <div>Nosotros</div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+
+      <div className="contenido">
+        <img src={nosotros} alt="nosotros" />
+
+        <div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis doloribus obcaecati hic officiis maxime illum, quod reiciendis, neque iure dolores, vitae maiores sunt perspiciatis asperiores. Assumenda dolores accusamus suscipit cupiditate.
+            Vitae, sit. Minima temporibus harum, maxime error accusantium pariatur commodi eius voluptas, doloribus dolores quidem provident unde reiciendis dignissimos repellendus fugiat maiores est vero ex sapiente nesciunt modi neque. Veritatis.
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
 
