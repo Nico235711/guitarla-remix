caa86ce69e20d524a41f507e41b551bc80c31a88
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-02-26 14:07:02 -0300

    Filtrar Guitarras por URL desde Strapi

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,12 +1,18 @@
+import { useLoaderData } from "@remix-run/react"
+import { getGuitarra } from "~/models/guitarras.server"
 
 export async function loader({ params }) {
 
   const { guitarraUrl } = params
-  console.log(guitarraUrl);
-  return {}
+  const guitarra = await getGuitarra(guitarraUrl)
+  return guitarra
 }
 
 const Guitarra = () => {
+  
+  const guitarra = useLoaderData()
+  console.log(guitarra.data[0].attributes.nombre);
+
   return (
     <div>guitarras.$guitarraUrl</div>
   )
