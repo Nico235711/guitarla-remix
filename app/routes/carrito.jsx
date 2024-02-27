12df3b1f4456307cf63c3d46c9f115493fec97a9
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-02-27 17:25:47 -0300

    Añadiendo el Contenido y CSS

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,8 +1,40 @@
+import stylesCarrito from '~/styles/carrito.css'
+
+export function links() {
+
+  return [
+    { rel: "stylesheet", href: stylesCarrito }
+  ]
+}
+
+export function meta() {
+
+  return [
+    { charset: "utf-8" },
+    { title: "GuitarLA - Carrito de Compras" },
+    { viewport: "width=device-width,initial-scale=1.0" },
+    { descripcion: "Venta de Guitarras, música, blog, carrito de compras, tienda" }
+  ]
+}
 
 const Carrito = () => {
 
   return (
-    <div>Carrito</div>
+    <main className="contenedor">
+      <h2 className="heading">Carrito de Compras</h2>
+
+      <div className="contenido">
+        <div className="carrito">
+          <h2>Articulos</h2>
+        </div>
+      
+        <aside className="resumen">
+          <h3>Resumen del Pedido</h3>
+          <p>Total a Pagar: $</p>
+        </aside>
+      </div>
+
+    </main>
   )
 }
 
