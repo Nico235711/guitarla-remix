503bfc851f59b4f89381c223cd9ef49876a085f2
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-02-28 15:43:48 -0300

    Añadir un Select para Modificar las Cantidades

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -39,7 +39,15 @@ const Carrito = () => {
 
                   <div>
                     <p className="nombre">{producto.nombre}</p>
-                    <p className="cantidad">Cantidad: {producto.cantidad}</p>
+                    <p className="cantidad">Cantidad: </p>
+                    <select value={producto.cantidad} className='select'>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                    </select>
+
                     <p className="precio">$<span>{producto.precio}</span></p>
                     <p className="subtotal">SubTotal: $<span>{producto.precio * producto.cantidad}</span></p>
                   </div>
